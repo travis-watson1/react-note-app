4ebda8387b88794bb418c0188ac44c1816be08fd
Travis Watson <watsonjamest@gmail.com>
2019-08-13 08:38:07 -0500

    Create Nav.js and import to App.js

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { directive } from '@babel/types';
+
+class Nav extends React.Component {
+    render() {
+        return(
+        <div>
+            Nav Component
+        </div>
+        );
+    }
+}
+
+export default Nav;
