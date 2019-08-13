3ec27464cce00a0f5331e254c5cdff0478b1322a
Travis Watson <watsonjamest@gmail.com>
2019-08-13 14:17:43 -0500

    Add NoteCards

---
+++ src/components/NoteCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NoteCard extends React.Component{
+    render(){
+        return(
+            <div className="note-card-container">NoteCard Component</div>
+        );
+    }
+}
+
+export default NoteCard;
